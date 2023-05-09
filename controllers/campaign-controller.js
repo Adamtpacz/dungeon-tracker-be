8dc55d8535c7745702366b54cd241f4c1afae093
Adamtpacz <adamtpacz@gmail.com>
2023-05-09 16:01:04 -0400

    Stub out index, create, and detail controller functions for Campaign

---
+++ controllers/campaign-controller.js
@@ -0,0 +1,39 @@
+const express = require('express')
+const { Campaign } = require('../models')
+
+// Index
+async function index(req, res, next) {
+    try {
+        res.json(await Campaign.find({}))
+    } catch (err) {
+        res.status(400).json(err)
+    }
+}
+
+// Create
+async function create(req, res, next) {
+    try {
+        res.json(await People.create(req.body));
+    } catch (err) {
+        res.status(400).json(err);
+    }
+}
+
+// Show
+async function detail(req, res, next) {
+    try {
+        res.json(await People.findById(req.params.id));
+    } catch (err) {
+        res.status(400).json(err);
+    }
+}
+
+// Delete
+
+// Update
+
+module.exports = {
+    index,
+    create,
+    getOne: detail
+}
