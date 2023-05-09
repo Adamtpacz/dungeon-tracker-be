4047ca05c4c65e634d10006ef6e73e373c549fcc
Adamtpacz <adamtpacz@gmail.com>
2023-05-09 15:44:22 -0400

    Add test routes for campaign show, delete and update

--- routes/campaign-router.js
+++ routes/campaign-router.js
@@ -12,4 +12,20 @@ router.post("/", (req, res) =>  {
 	res.status(201).json({message: "campaign create route"})
 })
 
+// Campaign show route
+router.get("/:id", (req, res) => {
+	res.status(200).json({message: "campaign show route: " + req.params.id })
+});
+
+// Campaign delete route
+router.delete("/:id", (req, res) => {
+	res.status(200).json({message: "campaign delete route: " + req.params.id })
+});
+
+// Campaign update route
+router.put("/:id", (req, res) => {
+	console.log(req.body)
+	res.status(200).json({message: "campaign update route: " + req.params.id })
+});
+
 module.exports = router
