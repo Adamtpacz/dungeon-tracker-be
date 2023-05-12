ece2ad0c1e6407dd74474e0b8fdb705bc0e4059e
Adamtpacz <adamtpacz@gmail.com>
2023-05-11 20:29:39 -0400

    Create encounter index/create controller functions

---
+++ controllers/encounter-controller.js
@@ -0,0 +1,18 @@
+const { Encounter } = require('../models')
+
+async function index(req, res, next) {
+    try {
+        res.json(await Encounter.find({}))
+    } catch (err) {
+        res.status(400).json(err)
+    }
+}
+
+// Create
+async function create(req, res, next) {
+    try {
+        res.json(await Encounter.create(req.body));
+    } catch (err) {
+        res.status(400).json(err);
+    }
+}
